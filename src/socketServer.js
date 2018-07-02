0382288f90bd5ce9ccdaf4775b7777394c0afbb9
Derek Martin <derek.n.martin@gmail.com>
2018-07-02 19:48:00 -0400

    Don't need to emit/listen when client connects, already in socket object

--- src/socketServer.js
+++ src/socketServer.js
@@ -11,10 +11,10 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server)
 
 io.on('connection', (socket) => {
-  console.log('Someone Connect!', socket.id)
-  io.emit('isConnected', true)
+  console.log('Client Connect!')
+
   socket.on('disconnect', (reason) => {
-    console.log('Someone Disconnected!', reason)
+    console.log('Client Disconnected!', reason)
   })
   socket.on('currentBlackCard', (data) => {
     io.emit('serverBlackCard', data)
